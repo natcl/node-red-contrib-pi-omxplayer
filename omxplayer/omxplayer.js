200d1976de3e8c1276f74a77af4a1245c7d27935
Nathanaël Lécaudé <natcl@users.noreply.github.com>
2017-09-01 10:13:31 -0400

    First test

--- omxplayer/omxplayer.js
+++ omxplayer/omxplayer.js
@@ -1,11 +1,35 @@
-var pm2 = require('omxplayer-controll');
+const omxp = require('omxplayer-controll');
 
 module.exports = function(RED) {
     function omxControl(config) {
         RED.nodes.createNode(this, config);
         var node = this;
-        this.on('input', function(msg) {
 
+        var opts = {
+            'audioOutput': 'alsa:hw:1,0',
+            'blackBackground': false,
+            'disableKeys': true,
+            'disableOnScreenDisplay': true,
+            'disableGhostbox': true,
+            'subtitlePath': '',
+            'startAt': 0,
+            'startVolume': 0.8,
+            'closeOtherPlayers': true //Should close other players if necessary
+        };
+
+        this.on('input', function(msg) {
+            if (msg.payload == 'play') {
+                omxp.open('/home/pi/source/2017_mnk/openframeworks/myApps/MF-MNK-VideoArtnet/bin/data/video/test_1080.mov', opts);
+            }
+            if (msg.payload == 'playpause') {
+                omxp.playPause(function(err){
+                    if (err) node.error(err, msg);
+                });
+            }
+        });
+
+        omxp.on('finish', function() {
+            node.warn('============= Finished =============');
         });
     }
     RED.nodes.registerType("rpi-omxplayer", omxControl);
